9aa69f0857c9304611d2e3bf048d2e9fe1d80e3a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-11 15:50:04 +0200

    scritto codice in forma umana

---
+++ esercizio Mail/js/script.js
@@ -0,0 +1,11 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+//Bisogna chiedere all'utente la sua email.
+
+//Creare un ARRAY con all'interno una lista di email 
+
+//Controllare che la mail dell'utente sia nella lista delle email contenute nell'ARRAY
+
+//OUTPUT : Messaggio esito controllo quindi sia positivo che negativo.
